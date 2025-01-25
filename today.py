ce95268663d86a18079fd208d8f075afcb52d964
Jad Lahrichi <jad.lahrichi@gmail.com>
2025-01-25 16:14:32 +0100

    Fix style issues, should get a 'Good Style' now

--- today.py
+++ today.py
@@ -1,10 +1,12 @@
-'''
-This is the module docstring
-'''
+"""A module computing buddy pair names for the day"""
 
 
 def my_name_is():
-    return("lebgdecasa")
+    """A function that always returns my github username"""
+
+    return "lebgdecasa"
 
 def my_buddy_is():
-    return("amabrouka")
+    """A function that always returns my buddy's github username"""
+
+    return "amabrouka"
